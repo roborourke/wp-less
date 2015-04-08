449895b966d31f5924d34718913ec5fc7feb1156
spacedmonkey <spacedmonkey2@gmail.com>
2015-04-08 12:35:48 +0100

    Still requires you to create directory if it doesnt exist. Should not have been removed

--- wp-less.class.php
+++ wp-less.class.php
@@ -407,6 +407,8 @@ if ( !class_exists( 'wp_less' ) ) {
 
 			if ( $path ) {
 				$dir = apply_filters( 'wp_less_cache_path', path_join( $upload_dir[ 'basedir' ], 'wp-less-cache' ) );
+				// create folder if it doesn't exist yet
+				wp_mkdir_p( $dir );
 			} else {
 				$dir = apply_filters( 'wp_less_cache_url', path_join( $upload_dir[ 'baseurl' ], 'wp-less-cache' ) );
 			}
